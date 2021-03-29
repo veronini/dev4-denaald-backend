51c3410ac6e3d691b69042b1616f5a4efa38332a
Simon Vanherweghe <simon.vanherweghe@gmail.com>
2021-03-29 11:12:33 +0200

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'f9aa9f7ba9032bbbcdb0779734ac83dc'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '0301ac6a-e8af-429b-a376-00176225c13d'
+};
