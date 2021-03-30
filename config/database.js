1515541984e5608433475acc1840d7e3498a7c59
alteruser <alteruser@Veronicas-MacBook-Pro.local>
2021-03-30 14:12:03 +0200

    test to deploy

--- config/database.js
+++ config/database.js
@@ -7,7 +7,7 @@ module.exports = ({ env }) => ({
         client: 'mysql',
         host: env('DATABASE_HOST', '127.0.0.1'),
         port: env.int('DATABASE_PORT', 3306),
-        database: env('DATABASE_NAME', 'denaald'),
+        database: env('DATABASE_NAME', 'denaal'),
         username: env('DATABASE_USERNAME', 'root'),
         password: env('DATABASE_PASSWORD', 'devine4life'),
         ssl: env.bool('DATABASE_SSL', false),
