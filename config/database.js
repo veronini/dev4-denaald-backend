ff8af8bc7c91169fb00f62c2feff5de9122ad158
alteruser <alteruser@Veronicas-MacBook-Pro.local>
2021-03-30 13:05:11 +0200

    andere database local

--- config/database.js
+++ config/database.js
@@ -7,7 +7,7 @@ module.exports = ({ env }) => ({
         client: 'mysql',
         host: env('DATABASE_HOST', '127.0.0.1'),
         port: env.int('DATABASE_PORT', 3306),
-        database: env('DATABASE_NAME', 'strapi-blog'),
+        database: env('DATABASE_NAME', 'denaald'),
         username: env('DATABASE_USERNAME', 'root'),
         password: env('DATABASE_PASSWORD', 'devine4life'),
         ssl: env.bool('DATABASE_SSL', false),
